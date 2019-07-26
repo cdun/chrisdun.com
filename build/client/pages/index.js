c3cf78bf36897ca68ec9608fca68ef97bcb5e841
Chris Dun <chris.dun@charlottetilbury.com>
2019-07-26 14:14:30 +0100

    WIP => NextJS9

--- build/client/pages/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-exports.default = () => {
-    return (React.createElement("h1", null, "Hey!"));
-};
-//# sourceMappingURL=index.js.map
